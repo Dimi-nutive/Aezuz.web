84557e95ceb9cee10f97ef9f4e2dfdda513e44d8
Akul Chaudhary <84304117+Dimi-nutive@users.noreply.github.com>
2022-06-27 19:37:17 +0530

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,14 @@
+burger= document.querySelector('.burger')
+navbar= document.querySelector('.navbar')
+rightNav= document.querySelector('.rightNav')
+navList= document.querySelector('.nav-list')
+
+
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    rightNav.classList.toggle('v-class-resp');
+})
+
+
